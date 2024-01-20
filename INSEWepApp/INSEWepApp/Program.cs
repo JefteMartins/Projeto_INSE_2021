4cc10d0ec7f79c31030e578efd0aba7367e4163e
Jefté Martins <jefte.martins@hotmail.com>
2024-01-19 22:33:15 -0300

    alterando searchCount endpoint e passando para front com axios

--- INSEWepApp/INSEWepApp/Program.cs
+++ INSEWepApp/INSEWepApp/Program.cs
@@ -6,6 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -13,6 +24,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<INSE2021Context>();
 
 var app = builder.Build();
+
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- INSE_2021-front/src/App.jsx
+++ INSE_2021-front/src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { ChartsPie } from './components/charts-pie'
+import axios from 'axios';
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Faça a chamada à API usando Axios
+        const response = await axios.get('https://localhost:7291/api/Inse2021/SearchCount');
+        console.log(response);
+        // Extraia a contagem da resposta
+        const { data } = response;
+      } catch (error) {
+        console.error('Erro ao fazer a chamada à API:', error);
+      }
+    };
+  
+    fetchData();
+  }, []); // O array vazio [] faz com que o useEffect
+
   return (
     <>
-    <ChartsPie/>
+      <ChartsPie/>
     </>
   )
 }
