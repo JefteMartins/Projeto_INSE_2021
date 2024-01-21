7a9a77587943509a3c3693c2a4b8c3bde6ef0042
Jefté Martins <jefte.martins@hotmail.com>
2024-01-21 20:52:21 -0300

    Refactor imports in App.jsx and main.jsx

--- INSE_2021-front/src/App.jsx
+++ INSE_2021-front/src/App.jsx
@@ -1,14 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { ChartsPie } from "./components/charts-pie";
-import axios from "axios";
-import { VerticalBar } from "./components/charts-vertical-bar";
-import Spinner from "react-bootstrap/Spinner";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import MainContainer from "./components/main-container";
-import { AsyncSearch } from "./components/async-search";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { NavbarHome } from "./components/navbar";
 
 function App() {
