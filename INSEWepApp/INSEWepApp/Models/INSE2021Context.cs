f8290327ddfdc29c9885dbf9cc551beff03fdd4c
Jefté Martins <jefte.martins@hotmail.com>
2024-01-21 21:15:52 -0300

    Remove connection string warning in INSE2021Context.cs

--- INSEWepApp/INSEWepApp/Models/INSE2021Context.cs
+++ INSEWepApp/INSEWepApp/Models/INSE2021Context.cs
@@ -24,8 +24,8 @@ namespace INSEWepApp.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-O20DB8B\\MSSQLSERVER01;Database=INSE2021;Trusted_Connection=True;");
+
+                optionsBuilder.UseSqlServer("Server= { insira sua string de conexão};Database=INSE2021;Trusted_Connection=True;");
             }
         }
 
